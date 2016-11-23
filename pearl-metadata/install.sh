f9f79062839cd104d51ca27dea607d5accd4917b
johan gall <johan.gall@gmail.com>
2016-11-23 23:14:38 +0900

    moo

--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -1,12 +1,13 @@
 post_install() {
-    $(
-        info "installing basher, because basher links bins and mans etc. nicely"
+    # basher breaks stuff...
+cat <<EOF | sh -s
+        echo "installing basher, because basher links bins and mans etc. nicely"
         git clone https://github.com/basherpm/basher.git ~/.basher
         export PATH="$HOME/.basher/bin:$PATH"
         eval "$(basher init -)"
         basher update # in a subshell to capture some kind of latent exit
 
-        info "installing usual packages"
+        echo "installing usual packages"
         basher install sstephenson/bats           # tests in bash
         basher install jimeh/stub.sh              # stub bash
         basher install freakhill/scripts          # my script
@@ -17,10 +18,10 @@ post_install() {
         basher install shyiko/commacd             # ,(forward) ,,(back) ,,,(both)
         basher install tests-always-included/mo   # moustache templates in bash
 
-        info "running fzf install script"
+        echo "running fzf install script"
         ( $HOME/.basher/cellar/packages/junegunn/fzf/install )
 
-        info "make sur that our homemade ssh/scp scripts run fine"
+        echo "make sur that our homemade ssh/scp scripts run fine"
         mkdir -p $HOME/.ssh/config.0
         mkdir -p $HOME/.ssh/backups
         touch $HOME/.ssh/config.0/empty
@@ -28,53 +29,54 @@ post_install() {
         touch $HOME/.ssh/config
         chmod 600 $HOME/.ssh/config
 
-        [ "$(ls -A $HOME/.ssh/config.0)" ] && info "config.0 not empty" \
+        [ "$(ls -A $HOME/.ssh/config.0)" ] && echo "config.0 not empty" \
                 || cp $HOME/.ssh/config $HOME/.ssh/config.0/oldconfig
 
-        info "create the golang go folder"
+        echo "create the golang go folder"
         mkdir -p $HOME/go
 
-        info "link tmux and git config"
+        echo "link tmux and git config"
         rm -f $HOME/.tmux.conf
         rm -f $HOME/.gitconfig
         ln -s "$PEARL_PKGDIR/tmux.conf" $HOME/.tmux.conf
         ln -s "$PEARL_PKGDIR/gitconfig" $HOME/.gitconfig
 
-        info "adding bashrc source to bash_profile for ssh"
-        cat <<EOF > ~/.bash_profile
+        echo "adding bashrc source to bash_profile for ssh"
+        cat <<EOF2 > ~/.bash_profile
 [ -f ~/.bashrc ] && source ~/.bashrc
-EOF
+EOF2
 
-        info "install lein"
+        echo "install lein"
         pushd ~/.local/bin
         curl -fsSL https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > lein
         chmod +x lein
         ./lein
-    )
+EOF
 }
 
 pre_update() {
-    info "NYI"
+    echo "NYI"
 }
 
 post_update() {
-    $(
-        info "NYI"
+    # basher breaks stuff
+    cat <<EOF | bash -s
+        echo "NYI"
         basher update
         for p in `basher outdated`
         do
             basher upgrade $p
         done
-    )
+EOF
 }
 
 pre_remove() {
     unlink tmux "$PEARL_PKGDIR/tmux.conf"
     unlink git  "$PEARL_PKGDIR/gitconfig"
-    info "removing basher"
+    echo "removing basher"
     rm -fr ~/.basher
 }
 
 post_remove() {
-    info "NYI"
+    echo "NYI"
 }
